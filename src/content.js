60a1f46b5f15967988b7beb09446a13d239c7a4b
Igor Limansky <i.e.limansky@gmail.com>
2017-02-12 12:49:30 +0300

    antigardanov feature

--- src/content.js
+++ src/content.js
@@ -1,45 +1,62 @@
-var div_ids = ['profile_friends', 'profile_friends_online', 'l_fr', 'wd-wrapper-_topnews', 'l_msg'];
-console.log('SCRIPT');
+//var div_ids = ['profile_friends', 'profile_friends_online', 'l_fr', 'wd-wrapper-_topnews', 'l_msg'];
+
 
 function update_dom() {
-  console.log('SCRIPT REMOVE');
-  div_ids.forEach(function( x ) {
-    var el = document.getElementById(x);
-//    console.log('checkin ' + x + ' : ' + el);
-    if (el) {
-      console.log('removed ' + x);
+  const gardanovs = document.querySelectorAll('[data-peer="22904165"]');
+  if (gardanovs) {
+    gardanovs.forEach(function(el) {
       el.remove();
-    }
-  });
+    });
+  }
+  const previews = [].slice.call(document.getElementsByClassName('nim-dialog--text-preview'));
+  if (previews) {
+    previews.forEach(function(el) {
+      el.remove();
+    });
+  }
+  /*  div_ids.forEach(function( x ) {
+   var el = document.getElementById(x);
+   //    console.log('checkin ' + x + ' : ' + el);
+   if (el) {
+   console.log('removed ' + x);
+   el.remove();
+   }
+   });*/
 }
 
-function try_observe()
-{
+try {
+  update_dom();
+}
+catch (ex) {
+
+}
+
+function try_observe() {
   try {
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
     var list             = document.querySelector('body');
 
-    var observer = new MutationObserver(function( mutations ) {
-      mutations.forEach(function( mutation ) {
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
 //    console.log(mutation);
         if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-          console.log('UPDATE');
 //      console.log(mutation);
           update_dom();
         }
       });
     });
 
-    observer.observe(list, {
-      childList:     true,
-      subtree:       true,
-      attributes:    true,
-      characterData: true
+    observer.observe(document.body, {
+      childList :     true,
+      subtree :       true,
+      attributes :    true,
+      characterData : true
     });
   }
   catch (ex) {
-    console.log(ex);
-    setTimeout(try_observe, 500);
+    console.error(ex);
+    update_dom();
+    setTimeout(try_observe, 100);
   }
 }
 try_observe();
